d80fcf3bfac260a8c33c5eab4a1fd5d7b374a9c4
sumit parihar <sumit@chowis.com>
2024-09-01 15:22:55 +0530

    new Tests

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,9 +1,14 @@
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
-  describe ".add" do
-    it "returns 0 for an empty string" do
-      expect(StringCalculator.add("")).to eq(0)
-    end
-  end
+	describe ".add" do
+		it "returns 0 for an empty string" do
+			expect(StringCalculator.add("")).to eq(0)
+		end
+
+		it "returns the number itself if only one number is provided" do
+			expect(StringCalculator.add("1")).to eq(1)
+			expect(StringCalculator.add("2")).to eq(2)
+		end
+	end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,8 @@
 class StringCalculator
 	def self.add(numbers)
-		0
+		return 0 if numbers.empty?
+
+		number_array = numbers.split(',')
+		number_array.map(&:to_i).reduce(:+)
 	end
 end
