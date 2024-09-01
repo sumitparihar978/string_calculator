c6dd7cc6be0ce23b39874e0a9cf0dc19b7804290
sumit parihar <sumit@chowis.com>
2024-09-01 15:27:58 +0530

    Allow the add method to handle any amount of numbers.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe StringCalculator do
 			expect(StringCalculator.add("1")).to eq(1)
 			expect(StringCalculator.add("2")).to eq(2)
 		end
+
+		it "return the sum of two numbers" do
+			expect(StringCalculator.add("1,2")).to eq(3)
+		end
+
+		it "returns the sum of multiple numbers" do
+			expect(StringCalculator.add("1,2,3,4,5,6")).to eq(21)
+		end
 	end
 end
